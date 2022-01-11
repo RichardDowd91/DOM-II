4fb42d2f5af11d99e23e061a8c8e4157a7bc75a1
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2022-01-11 11:48:25 -0800

    added two more events

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ import './less/index.less'
 const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseenter', function (evt) {
     evt.target.style.color = 'purple';
-})
+});
+
+const nav = document.querySelector('.nav');
+nav.addEventListener('dblclick', function(evt) {
+    console.log('double click event')
+});
+
+const log = document.querySelector('body');
+log.addEventListener('keydown', logKey);
+
+function logKey(evt) {
+    console.log(log)
+}
