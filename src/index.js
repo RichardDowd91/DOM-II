4287f07ef025488b1956200fe27f28fa1a179ef3
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2022-01-11 11:23:31 -0800

    have my first event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './less/index.less'
 
 // Your code goes here!
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseenter', function (evt) {
+    evt.target.style.color = 'purple';
+})
